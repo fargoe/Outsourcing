d8957495372e98b8a8f6929f385fe5566bcfcb16
fargoe <fargoewave@gmail.com>
2024-09-19 00:46:44 +0900

    init

---
+++ src/main/java/com/sparta/outsourcing/OutsourcingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.outsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingApplication.class, args);
+    }
+
+}
