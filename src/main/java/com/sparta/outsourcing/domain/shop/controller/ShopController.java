b902cf682f6214b171b93c8b53f4b64382aa99f7
Robbie <Robbie@email.com>
2024-09-20 20:37:49 +0900

    [Fix]가게생성 컨트롤러 수정

--- src/main/java/com/sparta/outsourcing/domain/shop/controller/ShopController.java
+++ src/main/java/com/sparta/outsourcing/domain/shop/controller/ShopController.java
@@ -3,8 +3,11 @@ package com.sparta.outsourcing.domain.shop.controller;
 import com.sparta.outsourcing.domain.shop.dto.ShopRequestDto;
 import com.sparta.outsourcing.domain.shop.dto.ShopResponseDto;
 import com.sparta.outsourcing.domain.shop.service.ShopService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/shops")
+@Validated
 public class ShopController {
     private final ShopService shopService;
 
     // 가게 생성
     @PostMapping
-    public ResponseEntity<ShopResponseDto> createShop(@RequestBody ShopRequestDto shopRequest) {
+    public ResponseEntity<ShopResponseDto> createShop(@RequestBody @Valid ShopRequestDto shopRequest) {
         ShopResponseDto createdShop = shopService.createShop(shopRequest);
-        return ResponseEntity.ok(createdShop);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdShop); // 201 Created 응답
     }
 }
