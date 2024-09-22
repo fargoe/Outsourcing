cc6057c81eab4ad08eff3a5d8f71199736e4aa61
fargoe <fargoewave@gmail.com>
2024-09-22 12:41:34 +0900

    [CHORE] 불필요한 쿼리 삭제

--- src/main/java/com/sparta/outsourcing/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/order/repository/OrderRepository.java
@@ -2,15 +2,12 @@ package com.sparta.outsourcing.domain.order.repository;
 
 import com.sparta.outsourcing.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("SELECT o FROM Order o WHERE o.shop.id = :shopId")
     List<Order> findByShopId(@Param("shopId") Long shopId);
 
     List<Order> findByUserId(Long userId);
