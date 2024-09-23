5239133a413b6261739a6e22fa4783aad4e2afaf
fargoe <fargoewave@gmail.com>
2024-09-23 09:23:04 +0900

    [FIX] MenuRepository 쿼리메서드 추가

--- src/main/java/com/sparta/outsourcing/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/repository/MenuRepository.java
@@ -3,8 +3,11 @@ package com.sparta.outsourcing.domain.menu.repository;
 import com.sparta.outsourcing.domain.menu.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     List<Menu> findByShopIdAndStatus(Long shopId, String status);
+    Optional<Menu> findByShopIdAndId(Long shopId, Long id);
 }
+
