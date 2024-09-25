53f5c15d52087ffa30ad94b8ad0beff63c7db356
fargoe <fargoewave@gmail.com>
2024-09-25 11:34:28 +0900

    [ADD] 유효성 검증 추가

--- src/main/java/com/sparta/outsourcing/domain/order/dto/OrderRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/order/dto/OrderRequestDto.java
@@ -1,13 +1,22 @@
 package com.sparta.outsourcing.domain.order.dto;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class OrderRequestDto {
-    private final Long shopId;
-    private final Long menuId;
-    private final String address;
-    private final String phoneNumber;
+
+    private Long shopId;
+    private Long menuId;
+
+    @NotNull(message = "주소는 필수 값입니다.")
+    @NotBlank(message = "주소는 공백일 수 없습니다.")
+    private String address;
+
+    @NotNull(message = "전화번호는 필수 값입니다.")
+    @NotBlank(message = "전화번호는 공백일 수 없습니다.")
+    private String phoneNumber;
 }
--- src/main/java/com/sparta/outsourcing/domain/order/dto/OrderStatusRequestDto.java
+++ src/main/java/com/sparta/outsourcing/domain/order/dto/OrderStatusRequestDto.java
@@ -1,11 +1,13 @@
 package com.sparta.outsourcing.domain.order.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class OrderStatusRequestDto {
+    @NotBlank
     private String newStatus;
 
     public OrderStatusRequestDto(String newStatus) {
